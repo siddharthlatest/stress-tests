54c3691c9373caf58ba4ca46496facccbe5873ff
unknown <sagar.chandarana123@gmail.com>
2014-11-12 14:50:53 -0200

    tests for appbase: patch, setEdge at different levels

---
+++ tests/appbase.py
@@ -0,0 +1,35 @@
+from locust import HttpLocust, TaskSet, task
+
+class tasks(TaskSet):
+    #define at what level to perform the operations
+    atLevel = 10
+    
+    #prepration: no need to touch
+    URL = ''
+    for x in xrange(1, atLevel + 1) :
+      URL = URL + '/l' + str(x)
+    
+    # @task(weight), weight defines the weight of the task.
+    @task(1)
+    def patchProperties(self):
+        self.client.patch( self.URL + "/~properties", 
+                           '{"data": {"property1": "value1", "property2":"value2", "property3": "value3"}, "secret": "aaf7c4cf139253df02cc23b5f67c076f"}',
+                           headers = { 'Content-Type': 'application/json'},
+                           name = "patch at level:" + str(self.atLevel)
+                         )
+    @task(1)
+    def setEdge(self):
+        self.client.patch( self.URL + "/~edges", 
+                           '{"data":  {"test_edge":{"path":"testing/l1", "order": null}}, "secret": "aaf7c4cf139253df02cc23b5f67c076f"}',
+                           headers = { 'Content-Type': 'application/json'},
+                           name = "set at level:" + str(self.atLevel)
+                         )
+
+class WebsiteUser(HttpLocust):
+    # base URL
+    host = "https://api.appbase.io/stress_test/v2/testing"
+    task_set = tasks
+
+    # Waiting before consecutive requests
+    min_wait = 100
+    max_wait = 5000
